2178c140512cc269031f73691b12ab506d41e708
Kislay Sinha <skislay7@gmail.com>
2022-12-24 19:26:26 +0530

    Added solution - LeetHub

--- Sum of Binary Tree - GFG/sum-of-binary-tree.java
+++ Sum of Binary Tree - GFG/sum-of-binary-tree.java
@@ -77,14 +77,14 @@ class GfG{
 
 class BinaryTree
 {
-
     static int sumBT(Node root){
-        if(root==null)
+       if(root==null)
             return 0;
         
         int ls=sumBT(root.left);
         int rs=sumBT(root.right);
+        
         return ls+rs+root.data;
-
+        
     }
 }
