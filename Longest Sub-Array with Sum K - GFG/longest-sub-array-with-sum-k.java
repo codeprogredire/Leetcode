436759ac8ab4d5d826612e3612b2f9aa9b0b3079
Kislay Sinha <skislay7@gmail.com>
2023-01-03 17:52:45 +0530

    Added solution - LeetHub

---
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int A[], int N, int K) {
+        //Complete the function
+        Map<Integer,Integer>hm=new HashMap<>();
+        int prefixSum=0;
+        int ans=0;
+        
+        for(int i=0;i<N;i++){
+            prefixSum+=A[i];
+            
+            if(prefixSum==K){
+                ans=Math.max(ans,i+1);
+            }
+            
+            int key=prefixSum-K;
+            
+            if(hm.containsKey(key))
+                ans=Math.max(ans,i-hm.get(key));
+            
+            if(!hm.containsKey(prefixSum))
+                hm.put(prefixSum,i);
+        }
+        
+        return ans;
+    }
+    
+    
+}
+
+
